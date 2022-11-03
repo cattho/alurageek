e966bc00e53185814dcfef1d4ccf4c946ba92cfa
Juan Giraldo <jusegiraldo76@gmail.com>
2022-11-03 15:15:30 -0500

    structure fixes

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
 const Login = () => {
+
+  const handleInputchange = e => {
+    console.log(e.target.nombre.value);
+    console.log(e.target.pass.value);
+    e.preventDefault();
+  };
+
   return (
     <div className='login-container'>
-        <h3 className='login-title'>Iniciar sesion</h3>
-        <form className='login-form'>
-            <input type="text" className='input-login-name' />
-            <input type="password" className='input-login-pass' />
-            <button className='btn-login-form'>Entrar</button>
-        </form>
+      <h3 className='login-title'>Iniciar sesion</h3>
+      <form className='login-form' onSubmit={handleInputchange}>
+        <input type="text" className='input-login-name' name='nombre' />
+        <input type="password" className='input-login-pass' name='pass' />
+        <button className='btn-login-form'>Entrar</button>
+      </form>
     </div>
   )
 }
